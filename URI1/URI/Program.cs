8b272202b5fe7980b644f6d4f197ff1288954938
Paola Garbato <paolagarbato@gmail.com>
2021-03-05 21:18:43 -0300

    desafio 1035

--- URI1/URI/Program.cs
+++ URI1/URI/Program.cs
@@ -75,6 +75,25 @@ namespace URI
 
             //Console.WriteLine($"TOTAL = R$ {(salario + (total*0.15)).ToString("F2").Replace(",", ".")}");
 
+
+
+            //1035:
+            //string[] selections = Console.ReadLine().Split(' ');
+            //int A = int.Parse(selections[0]);
+            //int B = int.Parse(selections[1]);
+            //int C = int.Parse(selections[2]);
+            //int D = int.Parse(selections[3]);
+
+            //if (B > C && D > A && (C + D > A + B) && C > 0 && D > 0 && (A % 2 == 0))
+            //{
+            //    Console.WriteLine("Valores aceitos");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Valores nao aceitos");
+            //}
+
+
         }
     }
 }
